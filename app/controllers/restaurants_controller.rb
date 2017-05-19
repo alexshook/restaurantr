2ca6128fc8ee5ea895284c9117bf74b64d45e565
alexshook <alexandra.shook@gmail.com>
2017-05-19 01:11:31 -0400

    Add an error message if no restaurants are found

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,11 @@ class RestaurantsController < ApplicationController
   def index
     params.permit(:cuisine)
     return unless cuisine
-    @restaurants = Restaurant.cuisine(cuisine).recently_graded.first(10)
+
+    @results = Restaurant.cuisine(cuisine).recently_graded.first(10)
+
+    @results.presence or
+      @error_message = error_message
   end
 
   private
@@ -10,5 +14,9 @@ class RestaurantsController < ApplicationController
   def cuisine
     return unless params[:cuisine].presence
     params[:cuisine].strip
+  end
+
+  def error_message
+    "Sorry, no restaurants found. Please try another type of cuisine!"
   end
 end
