d4aabcf1bbb31ec1b162253c062a70a8dedbb4e6
alexshook <alexandra.shook@gmail.com>
2017-05-18 20:07:03 -0400

    Add scopes for Thai and recently graded
    - Adds a convenience scope for Thai restaurants.
    - Adds a convenience scope for ordering restaurants by grade_date
      descending.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   enum grade: %w(a b)
+
+  scope :thai, -> { where(cuisine: "Thai") }
+  scope :recently_graded, -> { order(grade_date: :desc) }
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,50 @@ describe Restaurant do
       expect(Restaurant.grades.keys).to eq(["a", "b"])
     end
   end
+
+  describe ".thai" do
+    let!(:thai_restaurant) do
+      Restaurant.create!(
+        cuisine: "Thai",
+        name: "Some Restaurant",
+        grade: "a"
+      )
+    end
+
+    let!(:bakery) do
+      Restaurant.create!(
+        cuisine: "Bakery",
+        name: "Some Bakery",
+        grade: "a"
+      )
+    end
+
+    it "returns a Thai restaurant" do
+      expect(Restaurant.thai).to eq([thai_restaurant])
+    end
+  end
+
+  describe ".recently_graded" do
+    let!(:thai_restaurant) do
+      Restaurant.create!(
+        cuisine: "Thai",
+        name: "Some Restaurant",
+        grade: "a",
+        grade_date: Time.now
+      )
+    end
+
+    let!(:bakery) do
+      Restaurant.create!(
+        cuisine: "Bakery",
+        name: "Some Bakery",
+        grade: "a",
+        grade_date: Time.now.yesterday
+      )
+    end
+
+    it "orders restaurants by grade_date descending" do
+      expect(Restaurant.recently_graded).to eq([thai_restaurant, bakery])
+    end
+  end
 end
